ee4579962cb82b8a5b2f4822d518f851470b0152
Prateek Darmwal <prateek.darmwal@soarlogic.com>
2017-12-22 00:12:36 +0530

    Added pagination feature

--- js/google-sheets-html.js
+++ js/google-sheets-html.js
@@ -27,17 +27,44 @@ function handleQueryResponse(response) {
     }
     var data = response.getDataTable();
     visualization = new google.visualization.Table(document.getElementById('table'));
-    visualization.draw(data, {
-        legend: 'bottom'
-    });
-    setTimeout(function(){
-        // $('#table').find('table').dataTable( {
-        //     "columnDefs": [
-        //       { "type": "num-fmt", "targets": 6 }
-        //     ]
-        //   } );
+    // for google table charts
+    // google.charts.load('current');   // Don't need to specify chart libraries!
+    //   google.charts.setOnLoadCallback(drawVisualization);
+    //   function drawVisualization() {
+    //     google.visualization.drawChart({
+    //      "containerId": "visualization_div",
+    //      "dataSourceUrl": "https://spreadsheets.google.com/a/google.com/tq?key=1ex_r8XFU8VKiTquIUoRww_xeUieDHJyyc7n7D_4Tt_M&pub=1",
+    //      "query":'SELECT A, B, C, D, E, F, G, H, I, J, K label A "Company Name", B "Token Created Name", C "Date When ICO Closed(Finished)", D "Token Type Raised(ETH, BTC, EOS...)", E "ETH raised", F "ETH price at ICO end", G "BTC raised", H "BTC price at ICO end", I "Other token raised USD value or token value", J "ETH value in USD at ICO end", K "BTC value in USD at ICO end"',
+    //      "chartType": "Table",
+    //      "options": {
+    //         "alternatingRowStyle": true,
+    //         "showRowNumber" : true,
+	//     "page": 'enable',
+    //         "pageSize": 20,
+    //         "pagingSymbols": {
+    //         "prev": 'prev',
+    //         "next": 'next'
+    //     },
+    //     pagingButtonsConfiguration: 'auto'
 
-        paginateTable();
-    }, 2000)
+    //      }
+    //    });
+    //   }
+    // google.charts.setOnLoadCallback(drawVisualization);
+    visualization.draw(data, {
+        legend: 'bottom',
+        "alternatingRowStyle": true,
+            // "showRowNumber" : true,
+	    "page": 'enable',
+            "pageSize": 10,
+            "pagingSymbols": {
+            "prev": 'prev',
+            "next": 'next'
+            }
+    });
+    // setTimeout(function(){
+    //     paginateTable(10);
+    //     $('#pagination-selector').show()
+    // }, 2000)
 }
 google.setOnLoadCallback(drawVisualization);
--- js/main.js
+++ js/main.js
@@ -1,67 +1,88 @@
-function paginateTable(row_per_page){
+(function($) {
+    $(function() {
+        $.widget("zpd.paging", {
+            options: {
+                limit: 5,
+                rowDisplayStyle: 'block',
+                activePage: 0,
+                rows: []
+            },
+            _create: function() {
+                var rows = $("tbody", this.element).children();
+                this.options.rows = rows;
+                this.options.rowDisplayStyle = rows.css('display');
+                var nav = this._getNavBar();
+                this.element.after(nav);
+                this.showPage(0);
+            },
+            _getNavBar: function() {
+                var rows = this.options.rows;
+                var nav = $('<div>', {class: 'paging-nav'});
+                for (var i = 0; i < Math.ceil(rows.length / this.options.limit); i++) {
+                    this._on($('<a>', {
+                        href: '#',
+                        text: (i + 1),
+                        "data-page": (i)
+                    }).appendTo(nav),
+                            {click: "pageClickHandler"});
+                }
+                //create previous link
+                this._on($('<a>', {
+                    href: '#',
+                    text: '<<',
+                    "data-direction": -1
+                }).prependTo(nav),
+                        {click: "pageStepHandler"});
+                //create next link
+                this._on($('<a>', {
+                    href: '#',
+                    text: '>>',
+                    "data-direction": +1
+                }).appendTo(nav),
+                        {click: "pageStepHandler"});
+                return nav;
+            },
+            showPage: function(pageNum) {
+                var num = pageNum * 1; //it has to be numeric
+                this.options.activePage = num;
+                var rows = this.options.rows;
+                var limit = this.options.limit;
+                for (var i = 0; i < rows.length; i++) {
+                    if (i >= limit * num && i < limit * (num + 1)) {
+                        $(rows[i]).css('display', this.options.rowDisplayStyle);
+                    } else {
+                        $(rows[i]).css('display', 'none');
+                    }
+                }
+            },
+            pageClickHandler: function(event) {
+                event.preventDefault();
+                $(event.target).siblings().attr('class', "");
+                $(event.target).attr('class', "selected-page");
+                var pageNum = $(event.target).attr('data-page');
+                this.showPage(pageNum);
+            },
+            pageStepHandler: function(event) {
+                event.preventDefault();
+                //get the direction and ensure it's numeric
+                var dir = $(event.target).attr('data-direction') * 1;
+                var pageNum = this.options.activePage + dir;
+                //if we're in limit, trigger the requested pages link
+                if (pageNum >= 0 && pageNum < this.options.rows.length) {
+                    $("a[data-page=" + pageNum + "]", $(event.target).parent()).click();
+                }
+            }
+        });
+    });
+})(jQuery);
 
-// get the table element
-var table = $('#table').find('table')[0],
-// number of rows per page
-n = row_per_page,
-// number of rows of the table
-rowCount = table.rows.length,
-// get the first cell's tag name (in the first row)
-firstRow = table.rows[0].firstElementChild.tagName,
-// boolean var to check if table has a head row
-hasHead = (firstRow === "TH"),
-// an array to hold each row
-tr = [],
-// loop counters, to start count from rows[1] (2nd row) if the first row has a head tag
-i,ii,j = (hasHead)?1:0,
-// holds the first row if it has a (<TH>) & nothing if (<TD>)
-th = (hasHead?table.rows[(0)].outerHTML:"");
-// count the number of pages
-var pageCount = Math.ceil(rowCount / n);
-// if we had one page only, then we have nothing to do ..
-if (pageCount > 1) {
-    // assign each row outHTML (tag name & innerHTML) to the array
-    for (i = j,ii = 0; i < rowCount; i++, ii++)
-        tr[ii] = table.rows[i].outerHTML;
-    // create a div block to hold the buttons
-    table.insertAdjacentHTML("afterend","<div id='buttons'></div");
-    // the first sort, default page is the first one
-    sort(1);
+function paginateTable(limit){
+    $('#table').find('table').paging({limit:limit});
 }
 
-// (p) is the selected page number. it will be generated when a user clicks a button
-function sort(p) {
-    /* create (rows) a variable to hold the group of rows
-    ** to be displayed on the selected page,
-    ** (s) the start point .. the first row in each page, Do The Math
-    */
-    var rows = th,s = ((n * p)-n);
-    for (i = s; i < (s+n) && i < tr.length; i++)
-        rows += tr[i];
-    
-    // now the table has a processed group of rows ..
-    table.innerHTML = rows;
-    // create the pagination buttons
-    document.getElementById("buttons").innerHTML = pageButtons(pageCount,p);
-    // CSS Stuff
-    document.getElementById("id"+p).setAttribute("class","active");
-}
 
+// Event listener for select value change.
 
-// (pCount) : number of pages,(cur) : current page, the selected one ..
-function pageButtons(pCount,cur) {
-    /* this variables will disable the "Prev" button on 1st page
-       and "next" button on the last one */
-    var prevDis = (cur == 1)?"disabled":"",
-        nextDis = (cur == pCount)?"disabled":"",
-        /* this (buttons) will hold every single button needed
-        ** it will creates each button and sets the onclick attribute
-        ** to the "sort" function with a special (p) number..
-        */
-        buttons = "<input type='button' value='&lt;&lt; Prev' onclick='sort("+(cur - 1)+")' "+prevDis+">";
-    for (i=1; i<=pCount;i++)
-        buttons += "<input type='button' id='id"+i+"'value='"+i+"' onclick='sort("+i+")'>";
-    buttons += "<input type='button' value='Next &gt;&gt;' onclick='sort("+(cur + 1)+")' "+nextDis+">";
-    return buttons;
-}
-}
+$(document).on('change','#pagination-selector', function() {
+    paginateTable(parseInt($('#pagination-selector').val()))
+})
